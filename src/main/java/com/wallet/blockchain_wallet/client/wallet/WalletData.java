15680ae15ac0a35568baa26aa2d9a1542c161285
BlaiseCz <blazej.czekala@gmail.com>
2020-11-08 21:20:35 +0100

    NotifyWallet added

--- src/main/java/com/wallet/blockchain_wallet/client/wallet/WalletData.java
+++ src/main/java/com/wallet/blockchain_wallet/client/wallet/WalletData.java
@@ -3,14 +3,11 @@ package com.wallet.blockchain_wallet.client.wallet;
 import lombok.Data;
 import lombok.Getter;
 
-import java.security.PrivateKey;
-import java.security.PublicKey;
 
 @Data
 @Getter
-public class    WalletData {
-    private PrivateKey privateKey;
-    private PublicKey publicKey;
-    private String hash = "";
-    private Transaction lastTransaction;
+public class WalletData {
+    private String privateKey;
+    private String publicKey;
+    private String walletAddress;
 }
